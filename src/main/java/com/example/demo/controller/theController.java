c1133b66267f4b2405322d48a38d3af2e1f16d5a
smy <smyaseen164@gmail.com>
2021-02-03 22:45:07 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/controller/theController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greeting")
+public class theController {
+
+    @RequestMapping(value= "", method = RequestMethod.GET)
+    public String sayHello() {
+        System.out.println("hi");
+        return "<h1>Hello<br>World</h1>";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "<form action=\"/greeting/user_greeting\" method=\"GET\">\n" +
+                "  <label for=\"fname\">First name:</label><br>\n" +
+                "  <input type=\"text\" id=\"fname\" name=\"fname\"><br>\n" +
+                "  <label for=\"lname\">Last name:</label><br>\n" +
+                "  <input type=\"text\" id=\"lname\" name=\"lname\"><br><br>\n" +
+                "  <input type=\"submit\" value=\"Submit\">\n" +
+                "</form>";
+    }
+
+    @RequestMapping(value = "/user_greeting", method = RequestMethod.GET)
+    public String sayHelloAgain(@RequestParam String fname, @RequestParam String lname) {
+        return "Hey! " + fname + " " + lname ;
+    }
+
+    @RequestMapping(value = "/order/{id}", method = RequestMethod.GET)
+    public String sayHelloAgain(@PathVariable String id) {
+        return "Hey! " + id ;
+    }
+
+
+
+}
